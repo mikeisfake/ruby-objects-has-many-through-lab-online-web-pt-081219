961ae46e7dd164cb7d2d388eb75748b9e2f44232
Mike Cooper <mikescooper@icloud.com>
2019-09-18 03:29:26 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,5 +1,16 @@
 class Patient 
   
+   @@all = []
   
+  attr_accessor :name
+  
+  def initialize(name) 
+    @name = name
+    @@all << self
+  end
+  
+  def self.all 
+    @@all 
+  end
   
 end 
